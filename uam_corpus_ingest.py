a5546142805f3a53bdcc35af49ed5eb198c056ae
simon <simondrury2010@gmail.com>
2026-09-21 17:36:46 +0200

    UAM part 1

--- uam_corpus_ingest.py
+++ uam_corpus_ingest.py
@@ -1,1 +1,10 @@
-print('hello')
+import xml.etree.ElementTree as ET
+from pathlib import Path
+import numpy as np
+
+class UAMCorpusIngest:
+    TRANSITIVITY_FIELD_MAP = {
+        'material': 0.85, 'behavioural': 0.40, 'mental': -0.30,
+        'verbal': -0.55, 'relational-attributive': 0.10,
+        'relational-identifying': 0.25, 'existential': -0.75,
+    }
